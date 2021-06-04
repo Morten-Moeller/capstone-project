c9caa387141687940d517273492a00ca36f284ca
Morten Möller <git@mortenm.de>
2021-06-04 15:54:52 +0200

    Add a custom useAudio hook

---
+++ src/hooks/useAudio.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+export default function useAudio(url) {
+  const [song] = useState(new Audio(url))
+  const [playing, setPlaying] = useState(false)
+
+  useEffect(() => {
+    playing ? song.play() : song.pause()
+  }, [playing])
+
+  useEffect(() => {
+    song.addEventListener('ended', () => setPlaying(false))
+    return () => song.removeEventListener('ended', () => setPlaying(false))
+  }, [])
+
+  function toggle() {
+    setPlaying(!playing)
+  }
+
+  return [playing, toggle]
+}
