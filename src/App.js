78fd2f4a4e8429403aa9f48d07a2858c29b8dad6
Morten Möller <git@mortenm.de>
2021-06-04 15:55:28 +0200

    Add PlayPage to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components'
+import PlayPage from './pages/PlayPage'
 
 function App() {
-  return <Container></Container>
+  const url =
+    'https://audio-ssl.itunes.apple.com/itunes-assets/Music/12/f7/c9/mzm.hyfizvmg.aac.p.m4a'
+
+  return (
+    <Container>
+      <PlayPage url={url} />
+    </Container>
+  )
 }
 
 const Container = styled.main`
