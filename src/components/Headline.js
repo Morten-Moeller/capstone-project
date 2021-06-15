c38e13bf51225cfdcbe751aea86b86acb59d784e
Morten Möller <git@mortenm.de>
2021-06-15 13:45:31 +0200

    Refactor Heading to Headline and export a StyledComponent

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,11 +1,8 @@
 //@ts-check
 import styled from 'styled-components/macro'
 
-export default function Heading({ children }) {
-  return <Headline>{children}</Headline>
-}
-
 const Headline = styled.h1`
   font: 300;
   font-size: 1.75rem;
 `
+export default Headline
