c2cc5d3ab6e78b9995ad34ba8e4435f8893f6ac0
Morten Möller <git@mortenm.de>
2021-06-21 12:20:33 +0200

    Add aria-label for play button

--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -43,6 +43,7 @@ export default function PlayPage({
                   width="100"
                   height="100"
                   viewBox="0 0 102 102.194"
+                  aria-label="Play button"
                 >
                   <g transform="translate(1 1.101)">
                     <path
