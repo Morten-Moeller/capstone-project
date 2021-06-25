cfc35031e5d06a5cce0417bd07d9b6b12c3e5802
Morten Möller <git@mortenm.de>
2021-06-25 22:32:18 +0200

    useMultiplayer alpha1

--- src/hooks/UseMultiplayer.js
+++ src/hooks/UseMultiplayer.js
@@ -38,15 +38,54 @@ export default function UseMultiplayer() {
   } = useMqtt()
 
   const [isReady, setIsReady] = useState(false)
-  const [isHost, setIsHost] = useState(false)
+  const [isHost, setIsHost] = useState(null)
   const [isRight, setIsRight] = useState(false)
   const [selectedPlaylist, setSelectedPlaylist] = useState(null)
   const [room, setRoom] = useState(null)
   const [newAnswers, setNewAnswers] = useState(defaultAnswers)
+  const [userName, setUserName] = useState(null)
+  const [player, setPlayer] = useState(null)
+
+  useEffect(() => {
+    setNewPlaylist(selectedPlaylist)
+    connect()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    subscribe(room)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connect])
+
+  // host ping
+  useEffect(() => {
+    let interval
+    if (isHost) {
+      const message = { title: room, body: 'HostIsTaken' }
+      sendMessage(message)
+    }
+    if (isHost === null) {
+      interval = setInterval(sendIsHost, 1000)
+    } else {
+      clearInterval(interval)
+    }
+    return clearInterval(interval)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isHost])
+
+  useEffect(() => {
+    sendUrl(getNextUrl)
+  }, [getNextUrl])
+
+  useEffect(() => {
+    if (answers) {
+      sendAnswers(answers)
+    }
+  }, [answers])
 
   useEffect(() => {
     if (isRight) {
-      const message = { title: room, body: clientId + ' ' + isRight }
+      const message = { title: room, body: clientId + ',' + isRight }
       sendMessage(message)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -55,11 +94,17 @@ export default function UseMultiplayer() {
   //message listener
   useEffect(() => {
     switch (true) {
-      case /(http:|https:)+[^\s]+[\w]/.test(messages[0]):
-        setSongUrl(messages[0].match(/(www|http:|https:)+[^\s]+[\w]/))
+      case /(http:|https:)+[^\s]+[\w]/.test(messages[0].body):
+        setSongUrl(messages[0].body.match(/(www|http:|https:)+[^\s]+[\w]/))
         break
-      case /(answer)/.test(messages[0]):
-        handleAnswers(messages[0])
+      case /(answer)/.test(messages[0].body):
+        handleAnswers(messages[0].body)
+        break
+      case /(isReady)/.test(messages[0].body):
+        handleIsReady(messages[0].body)
+        break
+      case /(hostIsTaken)/.test(messages[0].body):
+        handleHost()
         break
       default:
         break
@@ -67,10 +112,28 @@ export default function UseMultiplayer() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [messages])
 
+  function handleHost() {
+    if (!isHost) setIsHost(false)
+  }
+
   function handleAnswers(rawAnswers) {
     const reformAnswers = rawAnswers.split(/(answer )/)
     const answers = JSON.parse(reformAnswers[2])
     setNewAnswers(answers)
+  }
+
+  function handleIsReady(rawMessage) {
+    const reformMessage = rawMessage.split(/(isAnswer)/)
+    const messageParts = reformMessage[2].split(',')
+    const playerName = messageParts[0]
+    const playerIsReady = messageParts[1]
+    console.log(playerName)
+    console.log(playerIsReady)
+
+    setPlayer([
+      ...player,
+      { playerName, playerIsReady: JSON.parse(playerIsReady) },
+    ])
   }
 
   function sendAnswers(answers) {
@@ -79,14 +142,40 @@ export default function UseMultiplayer() {
     sendMessage(message)
   }
 
+  function sendUrl(url) {
+    const message = { title: room, body: url }
+    sendMessage(message)
+  }
+
+  function sendIsHost() {
+    if (isHost === null) {
+      const message = { title: room, body: 'noHost' }
+      sendMessage(message)
+    }
+
+    function sendIsReady() {
+      const message = {
+        title: room,
+        body: 'isReady' + userName + ',' + isReady,
+      }
+      sendMessage(message)
+    }
+  }
+
   return {
     setIsReady,
     setIsHost,
     setIsRight,
+    setUserName,
+    setSelectedPlaylist,
+    setRoom,
     newAnswers,
     isPlaying,
     duration,
-    setSelectedPlaylist,
-    setRoom,
+    player,
+    toggleAudio,
+    stopAudio,
+    isLoaded,
+    initiateNextSong,
   }
 }
